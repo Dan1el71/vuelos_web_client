bae677f7e1f1c685d34c2a17a05f1765161e1ff7
Dan1el71 <ddrmhk1@gmail.com>
2024-11-21 16:09:50 -0500

    feat: add AppHeader component with navigation links and login button

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom'
+
+const AppHeader = () => {
+  const navItems = [
+    {
+      name: 'Reservar',
+      ref: '/',
+    },
+    {
+      name: 'Tus Reservas',
+      ref: '/reservas',
+    },
+    {
+      name: 'Contacto',
+      ref: '/contacto',
+    },
+  ]
+
+  return (
+    <header className="bg-secondary shadow-xl">
+      <div className="flex flex-row justify-between mx-auto max-w-6xl min-h-16">
+        <div className="mx-10 my-auto">
+          <h1 className="font-bold text-lg uppercase">Logo</h1>
+        </div>
+
+        <nav className="my-auto">
+          {navItems.map(({ name, ref }, i) => (
+            <Link
+              className="px-5 py-5 font-normal hover:font-bold hover:border-b-2 hover:border-b-red-900"
+              key={i}
+              to={ref}
+            >
+              {name}
+            </Link>
+          ))}
+          <Link to="/login" className="mx-4">
+            <button className="text-white text-sm font-medium border-blue-600 border px-3 py-[6px] bg-blue-600 rounded-sm hover:bg-blue-700">
+              Iniciar Sesión
+            </button>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default AppHeader
