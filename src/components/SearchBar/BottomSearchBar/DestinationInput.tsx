09f9a9fa52951b9ffade2ec21ffc997f621a5fef
DanielOrtiz08 <ddov0811@gmail.com>
2024-11-23 11:38:54 -0500

    agregando buscadores con opciones establecidoas para la ciudad de origen y destino, modificando redireccion de botones en el componente LinksMenu

--- src/components/SearchBar/BottomSearchBar/DestinationInput.tsx
+++ src/components/SearchBar/BottomSearchBar/DestinationInput.tsx
@@ -1,9 +1,135 @@
 import React, { useState } from "react";
 
 const DestinationInput: React.FC = () => {
+    const predefinedOptions = [
+        "Ciudad de México",
+        "Bogotá",
+        "Lima",
+        "Buenos Aires",
+        "Madrid",
+        "Nueva York",
+    ];
+
+    const [origin, setOrigin] = useState("");
+    const [filteredOriginOptions, setFilteredOriginOptions] = useState(predefinedOptions);
+    const [showOriginOptions, setShowOriginOptions] = useState(false);
+
+    const [destination, setDestination] = useState("");
+    const [filteredDestinationOptions, setFilteredDestinationOptions] = useState(predefinedOptions);
+    const [showDestinationOptions, setShowDestinationOptions] = useState(false);
+
+
+    const handleFilterOptions = (
+        value: string,
+        setFiltered: React.Dispatch<React.SetStateAction<string[]>>
+    ) => {
+        setFiltered(
+            predefinedOptions.filter((option) =>
+                option.toLowerCase().includes(value.toLowerCase())
+            )
+        );
+    };
+
+    const handleValidation = (
+        value: string,
+        setValue: React.Dispatch<React.SetStateAction<string>>
+    ) => {
+        if (!predefinedOptions.includes(value)) {
+            setValue("");
+        }
+    };
+
+
     return (
-        <></>
+        <div className="w-full px-1">
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+                Origen y destino
+            </label>
+            <div className="flex flex-col md:flex-row">
+                <div className="w-items-center mx-1 py-1 relative">
+                    <input
+                        type="text"
+                        value={origin}
+                        onChange={(e) => {
+                            setOrigin(e.target.value);
+                            handleFilterOptions(e.target.value, setFilteredOriginOptions);
+                            setShowOriginOptions(true);
+                        }}
+                        onBlur={() => {
+                            handleValidation(origin, setOrigin);
+                            setShowOriginOptions(false); // Oculta las opciones al perder foco
+                        }}
+                        onFocus={() => setShowOriginOptions(true)} // Muestra las opciones al enfocar
+                        placeholder="¿De dónde sales?"
+                        className="w-full py-3 px-4 border border-gray-300 rounded-lg text-sm shadow-sm
+              focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-150 ease-in-out
+              placeholder-gray-400"
+                    />
+                    {showOriginOptions && (
+                        <ul className="absolute top-full left-0 w-full bg-white border border-gray-300 rounded-lg shadow-md z-10">
+                            {filteredOriginOptions.length > 0 ? (
+                                filteredOriginOptions.map((option) => (
+                                    <li
+                                        key={option}
+                                        className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+                                        onMouseDown={() => setOrigin(option)} // MouseDown para evitar onBlur conflictivo
+                                    >
+                                        {option}
+                                    </li>
+                                ))
+                            ) : (
+                                <li className="py-2 px-4 text-gray-500 cursor-default">
+                                    Sin coincidencias
+                                </li>
+                            )}
+                        </ul>
+                    )}
+                </div>
+
+                <div className="w-items-center mx-1 py-1 relative">
+                    <input
+                        type="text"
+                        value={destination}
+                        onChange={(e) => {
+                            setDestination(e.target.value);
+                            handleFilterOptions(e.target.value, setFilteredDestinationOptions);
+                            setShowDestinationOptions(true);
+                        }}
+                        onBlur={() => {
+                            handleValidation(destination, setDestination);
+                            setShowDestinationOptions(false); // Oculta las opciones al perder foco
+                        }}
+                        onFocus={() => setShowDestinationOptions(true)} // Muestra las opciones al enfocar
+                        placeholder="¿Adónde vas?"
+                        className="w-full py-3 px-4 border border-gray-300 rounded-lg text-sm shadow-sm
+              focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-150 ease-in-out
+              placeholder-gray-400"
+                    />
+                    {showDestinationOptions && (
+                        <ul className="absolute top-full left-0 w-full bg-white border border-gray-300 rounded-lg shadow-md z-10">
+                            {filteredDestinationOptions.length > 0 ? (
+                                filteredDestinationOptions.map((option) => (
+                                    <li
+                                        key={option}
+                                        className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+                                        onMouseDown={() => setDestination(option)} // MouseDown para evitar onBlur conflictivo
+                                    >
+                                        {option}
+                                    </li>
+                                ))
+                            ) : (
+                                <li className="py-2 px-4 text-gray-500 cursor-default">
+                                    Sin coincidencias
+                                </li>
+                            )}
+                        </ul>
+                    )}
+                </div>
+            </div>
+
+        </div>
+
     );
-  };
-  
-  export default DestinationInput;
+};
+
+export default DestinationInput;
--- src/components/SearchBar/TopSearchBar/LinksMenu.tsx
+++ src/components/SearchBar/TopSearchBar/LinksMenu.tsx
@@ -1,33 +1,31 @@
 import React from "react";
 
 const LinksMenu: React.FC = () => {
-  return (
-    <div className="flex items-center gap-4 py-1">
-      <a
-        href="/aerolineas"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-full text-sm font-medium text-gray-700 shadow-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200"
-      >
-        Aerolíneas
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-        </svg>
-      </a>
+    return (
+        <div className="flex items-center gap-4 py-1">
+            <a
+                href="http://localhost:8080/api/v1/aerolineas"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-full text-sm font-medium text-gray-700 shadow-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200">
+                Aerolíneas
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                </svg>
+            </a>
 
-      <a
-        href="/todos-los-vuelos"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-full text-sm font-medium text-gray-700 shadow-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200"
-      >
-        Todos los vuelos
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-        </svg>
-      </a>
-    </div >
-  );
+            <a
+                href="http://localhost:8080/api/v1/vuelo"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-full text-sm font-medium text-gray-700 shadow-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200">
+                Todos los vuelos
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                </svg>
+            </a>
+        </div >
+    );
 };
 
 export default LinksMenu;
