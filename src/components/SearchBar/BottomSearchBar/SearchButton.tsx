e0d0c81b38507244ac943da6548ca0455c754d41
DanielOrtiz08 <ddov0811@gmail.com>
2024-11-23 11:52:19 -0500

    implementando un componente del boton de busqueda

--- src/components/SearchBar/BottomSearchBar/SearchButton.tsx
+++ src/components/SearchBar/BottomSearchBar/SearchButton.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const SearchButton: React.FC = () => {
-  return (
-    <></>
-    );
+	return (
+		<div>
+			<label htmlFor="">
+				  {/*(caracter vacio) no supe como bajar el botón*/}
+			</label>
+			<button
+				className="rounded-xl bg-gradient-to-t from-sky-400 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-md shadow-md transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+				onClick={() => alert('Búsqueda iniciada')}>
+				Buscar
+			</button>
+		</div>
+	);
 };
 
 export default SearchButton;
