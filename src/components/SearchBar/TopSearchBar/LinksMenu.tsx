de49c1de1f8518a1e8db9b6a5998df11b9565e93
Dan1el71 <ddrmhk1@gmail.com>
2024-11-24 11:14:34 -0500

    Update LinksMenu component to replace SVG icons with Bootstrap icons for improved consistency

--- src/components/SearchBar/TopSearchBar/LinksMenu.tsx
+++ src/components/SearchBar/TopSearchBar/LinksMenu.tsx
@@ -3,26 +3,24 @@ import { Link } from "react-router-dom";
 
 const LinksMenu: React.FC = () => {
     return (
-        <div className="flex items-center gap-4 py-1">
-            <Link
-                to="aerolineas"
-                className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-full text-sm font-medium text-gray-700 shadow-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200">
-                Aerolíneas
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                </svg>
-            </Link>
+      <div className="flex items-center gap-4 py-1">
+        <Link
+          to="aerolineas"
+          className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-full text-sm font-medium text-gray-700 shadow-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200"
+        >
+          Aerolíneas
+          <i className="bi bi-arrow-up-right"></i>
+        </Link>
 
-            <Link
-                to="vuelos"
-                className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-full text-sm font-medium text-gray-700 shadow-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200">
-                Todos los vuelos
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                </svg>
-            </Link>
-        </div >
-    );
+        <Link
+          to="vuelos"
+          className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-full text-sm font-medium text-gray-700 shadow-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200"
+        >
+          Todos los vuelos
+          <i className="bi bi-arrow-up-right"></i>
+        </Link>
+      </div>
+    )
 };
 
 export default LinksMenu;
