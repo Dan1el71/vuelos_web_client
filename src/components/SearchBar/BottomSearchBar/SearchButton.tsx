78134585f06176144fd1a5387b5ba09f689dda6b
Dan1el71 <ddrmhk1@gmail.com>
2024-11-24 11:15:47 -0500

    Remove redundant rounded class from SearchButton for cleaner styling

--- src/components/SearchBar/BottomSearchBar/SearchButton.tsx
+++ src/components/SearchBar/BottomSearchBar/SearchButton.tsx
@@ -4,10 +4,10 @@ const SearchButton: React.FC = () => {
 	return (
 		<div>
 			<label htmlFor="">
-				  {/*(caracter vacio) no supe como bajar el botón*/}
+				{/*(caracter vacio) no supe como bajar el botón*/}
 			</label>
 			<button
-				className="rounded-xl bg-gradient-to-t from-sky-400 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-md shadow-md transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+				className="rounded-xl bg-gradient-to-t from-sky-400 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 shadow-md transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
 				onClick={() => alert('Búsqueda iniciada')}>
 				Buscar
 			</button>
