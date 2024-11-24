3db61d08fa5bd48c08806d7fa3387c49602f9c3f
Dan1el71 <ddrmhk1@gmail.com>
2024-11-24 11:28:29 -0500

    Enhance SearchBar component by adding props for DatePicker labels and IDs for improved clarity and usability

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import DestinationInput from "./BottomSearchBar/DestinationInput";
-import DatePicker from "./BottomSearchBar/DatePicker";
-import GuestsSelector from "./BottomSearchBar/GuestsSelector";
-import SearchButton from "./BottomSearchBar/SearchButton";
-import TripTypeSelector from "./TopSearchBar/TripTypeSelector";
-import LinksMenu from "./TopSearchBar/LinksMenu";
+import React, { useState } from 'react'
+import DestinationInput from './BottomSearchBar/DestinationInput'
+import DatePicker from './BottomSearchBar/DatePicker'
+import GuestsSelector from './BottomSearchBar/GuestsSelector'
+import SearchButton from './BottomSearchBar/SearchButton'
+import TripTypeSelector from './TopSearchBar/TripTypeSelector'
+import LinksMenu from './TopSearchBar/LinksMenu'
 
 const SearchBar: React.FC = () => {
-  const [tripType, setTripType] = useState<"roundtrip" | "oneway">("roundtrip");
+  const [tripType, setTripType] = useState<'roundtrip' | 'oneway'>('roundtrip')
 
   return (
     <section className="rounded-lg shadow-lg">
@@ -18,12 +18,15 @@ const SearchBar: React.FC = () => {
 
       <div className="rounded-lg bg-gradient-to-t from-sky-200 to-cyan-50 flex flex-col md:flex-row items-center justify-between px-5 mt-4 py-4">
         <DestinationInput />
-        <DatePicker />
+        <DatePicker label="Fecha de entrada" id="check-in" />
+        {tripType === 'roundtrip' && (
+          <DatePicker label="Fecha de salida" id="check-out" />
+        )}
         <GuestsSelector />
         <SearchButton />
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default SearchBar;
+export default SearchBar
