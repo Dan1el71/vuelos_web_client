3a8dc6858044a86febbd960a7357d9502fd860fb
Dan1el71 <ddrmhk1@gmail.com>
2024-11-24 11:17:58 -0500

    Refactor SearchButton component to simplify structure and improve layout

--- src/components/SearchBar/BottomSearchBar/SearchButton.tsx
+++ src/components/SearchBar/BottomSearchBar/SearchButton.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from 'react'
 
 const SearchButton: React.FC = () => {
-	return (
-		<div>
-			<label htmlFor="">
-				{/*(caracter vacio) no supe como bajar el botón*/}
-			</label>
-			<button
-				className="rounded-xl bg-gradient-to-t from-sky-400 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 shadow-md transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
-				onClick={() => alert('Búsqueda iniciada')}>
-				Buscar
-			</button>
-		</div>
-	);
-};
+  return (
+    <div className='mt-6'>
+      <button
+        className="rounded-xl bg-gradient-to-t from-sky-400 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 shadow-md transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+        onClick={() => alert('Búsqueda iniciada')}
+      >
+        Buscar
+      </button>
+    </div>
+  )
+}
 
-export default SearchButton;
+export default SearchButton
