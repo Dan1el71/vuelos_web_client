26ad39f42090347a4e67f9c571d37a596f660a3c
Dan1el71 <ddrmhk1@gmail.com>
2024-11-26 16:50:47 -0500

    fix: Remove StrictMode wrapper from main render for simplified rendering

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,7 @@ import { createRoot } from 'react-dom/client'
 import '@styles/index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Router'
-import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />
-  </StrictMode>
+  <RouterProvider router={router} future={{ v7_startTransition: true }} />
 )
